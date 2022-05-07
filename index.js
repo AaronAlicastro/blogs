d611dbb2f07e856262cb0dbc67465f702edb31e8
Edward Laiton <edwlz@hotmail.com>
2022-05-06 19:33:06 -0500

    for, quitamos ruta que no estaba

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.set("views", __dirname+"/src/views");
 app.set("view engine", "ejs");
 
 mongoose.connect(
-    "mongodb+srv://Laiton785:LaitonMongo1@cluster0.vy8yi.mongodb.net/22_04Crud?retryWrites=true&w=majority"
+    "mongodb+srv://Laiton785:LaitonMongo1@cluster0.vy8yi.mongodb.net/Blogs_Data?retryWrites=true&w=majority"
 ).then(function (db) {
     console.log("Conectado con la base de datos");
 }).catch(function (err) {
@@ -19,17 +19,12 @@ mongoose.connect(
 const SkemaBlogs = require("./src/models/Blogs");
 
 app.get("/blogs",(req,res)=>{
-    res.render('blogs', { saludo:"hola" } );
+    var datos_blogs = SkemaBlogs.find();
+    res.render('blogs', {blogs : datos_blogs});
 });
 
 app.get("/verMas",(req,res)=>{
     res.render('verMas', { } );
-});
-app.get("/home", async (req,res)=>{
-    var datos_blogs = SkemaBlogs.find();
-    
-    res.render('blogs', {blogs : datos_blogs});
-
 });
 
 app.post('/editar', function(req,res){
